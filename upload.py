87e3297553c82f50df88386419d394dafeee3c26
Rakeshvaradi <85574876+Rakeshvaradi@users.noreply.github.com>
2021-08-12 19:29:37 +0530

    Add files via upload

---
+++ upload.py
@@ -0,0 +1,57 @@
+import sys
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+
+# Constants for IBM COS values
+COS_ENDPOINT = "https://s3.jp-tok.cloud-object-storage.appdomain.cloud" # Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
+COS_API_KEY_ID = "UxUdPxdtK4rrdmcQdnAx3TH4Pcqy3ERlkRd2-iQfLnjk" # eg "W00YixxxxxxxxxxMB-odB-2ySfTrFBIQQWanc--P3byk"
+COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/1a2afce41ec54745b591f9ed0945b8cf:bce31167-25a6-43b2-9d05-10f7c1a0571b::" # eg "crn:v1:bluemix:public:cloud-object-storage:global:a/3bf0d9003xxxxxxxxxx1c3e97696b71c:d6f04d83-6c4f-4a62-a165-696756d63903::"
+
+# Create client
+cos = ibm_boto3.client("s3",
+    ibm_api_key_id=COS_API_KEY_ID,
+    ibm_service_instance_id=COS_INSTANCE_CRN,
+    config=Config(signature_version="oauth"),
+    endpoint_url=COS_ENDPOINT
+)
+
+def upload_large_file(bucket_name, item_name, file_path):
+    print("Starting large file upload for {0} to bucket: {1}".format(item_name, bucket_name))
+
+    # set the chunk size to 5 MB
+    part_size = 1024 * 1024 * 5
+
+    # set threadhold to 5 MB
+    file_threshold = 1024 * 1024 * 5
+
+    # Create client connection
+    cos_cli = ibm_boto3.client("s3",
+        ibm_api_key_id=COS_API_KEY_ID,
+        ibm_service_instance_id=COS_INSTANCE_CRN,
+        config=Config(signature_version="oauth"),
+        endpoint_url=COS_ENDPOINT
+    )
+
+    # set the transfer threshold and chunk size in config settings
+    transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+        multipart_threshold=file_threshold,
+        multipart_chunksize=part_size
+    )
+
+    # create transfer manager
+    transfer_mgr = ibm_boto3.s3.transfer.TransferManager(cos_cli, config=transfer_config)
+    bucket_name = "smart-test"
+    upload_filename = sys.argv[1]
+    object_name = sys.argv[2]
+
+    try:
+        # initiate file upload
+        future=transfer_mgr.upload(upload_filename, bucket_name, object_name)
+        # wait for upload to complete
+        future.result()
+
+        print ("Large file upload complete!")
+    except Exception as e:
+        print("Unable to complete large file upload: {0}".format(e))
+    finally:
+        transfer_mgr.shutdown()
